8c48782ec93dba352175731a8c8476ebb9165f63
BenDickens <bendickens@zoho.com>
2020-04-01 17:11:20 +0200

    Individual patient data created

--- load.py
+++ load.py
@@ -5,8 +5,12 @@ import numpy as np
 
 def read():
     data = pd.read_csv(open('./dataset_mood_smartphone.csv'))
-    print(data)
-
+    for i in range(1,34):
+    	patientNo = str(i)
+    	patient = "AS14." + patientNo.zfill(2)
+    	patient_data = data.loc[data.id==patient]
+    	patient_data.to_csv("./patientData/patient" + patientNo + ".csv")
+        
 
 if __name__ == '__main__':
 	read()
