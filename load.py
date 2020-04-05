cf901517897ce1fd3c60ee7ae81d3426ed06b226
vivdenx <vivian.claes@live.nl>
2020-04-05 18:37:56 +0200

    Updated pre-processing

--- load.py
+++ load.py
@@ -3,12 +3,13 @@ import numpy as np
 
 
 def read():
-    data = pd.read_csv(open('./data/dataset_mood_smartphone.csv'))
+    data = pd.read_csv('./data/dataset_mood_smartphone.csv')
     patients = np.unique(list(data.id))
     for i in patients:
         patient_data = data.loc[data.id == i]
-        patient_data.to_csv("./patientData/patient" + i + ".csv")
-
+        filename = ".patientData/patient" + i + ".csv"
+        patient_data.to_csv(filename)
+        logging.info(f'Patient data written to {filename}.')
 
 if __name__ == '__main__':
     read()
--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,9 @@ import pandas as pd
 import os
 import logging
 
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    level=logging.DEBUG)
+
 def clean_time(in_filepath):
     """
     Cleans the time into a more readable format with different columns for
@@ -16,25 +19,30 @@ def clean_time(in_filepath):
                 inplace=True)
 
     # Converting the time to a more readable format.
-    for index, entry in data.iterrows():
-        date, time = entry["old_time"].split()
-        year, month, day = date.split("-")
+    try:
+        for index, entry in data.iterrows():
+            date, time = entry["old_time"].split()
+            year, month, day = date.split("-")
 
-        if time.endswith(":00.000") is True:
-            time = time.replace(":00.000", "")
-        data.at[index, "time"] = time
-        data.at[index, "month"] = month
-        data.at[index, "day"] = day
+            if time.endswith(":00.000") is True:
+                time = time.replace(":00.000", "")
+            data.at[index, "time"] = time
+            data.at[index, "month"] = month
+            data.at[index, "day"] = day
 
-    data = data.drop(columns="old_time")
-    data.to_csv(in_filepath)
+        data = data.drop(columns="old_time")
+        data.to_csv(in_filepath)
+        logging.info(f"File at {in_filepath} cleaned.")
+    except:
+        logging.info(f"File at {in_filepath} already cleaned.")
+
+
 
 
 def run():
     folderpath = "./patientData/"
     for path in os.listdir(folderpath):
         clean_time(folderpath + path)
-        # logging.info(f"File at {path} cleaned.")
 
 
 if __name__ == "__main__":
